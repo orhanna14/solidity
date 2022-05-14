eaa47f4d8d8364f4eb859ce61c6b24c8344e5ce2
Olivia McGoffin <orhanna14@gmail.com>
2022-05-14 07:58:50 -0700

    Add tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -20,6 +20,15 @@ beforeEach(async () => {
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+  it('updates a message', async () => {
+      await inbox.methods.setMessage('Bye there').send({from: accounts[0]});
+      const message = await inbox.methods.message().call();
+      assert.equal(message, 'Bye there');
   });
 });
